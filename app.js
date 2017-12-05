17d39a05e77f25b29740bbfbde4a448d8f0618df
RemyMozelle <timoz974@hotmail.fr>
2017-12-05 10:55:24 +0400

    ajout ejs ejs layout

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const dotenv = require('dotenv').config({ path: '.env' })
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
 
-app.set('view engine', 'pug');
-app.set('views', 'app/views/pages');
 
+app.set('view engine', 'ejs');
+app.set('views', './app/views/layouts');
+
+app.use(expressLayouts);
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
--- app/models/users.js
+++ app/models/users.js
@@ -1,7 +1,15 @@
-const database = require('../../config/database/database');
+const db = require('../../config/database/database');
 
 const users = {
+  getOne() {
+    db.getConnection().query('SELECT * FROM users', (err, users) => {
+      console.log(users)
+    });
+  },
+
   insertUsers() {
     database.getConnection().query('')
   }
 }
+
+module.exports = users;
