d5b8d4fa4f21facc60bdc3ccae5bd9d8c29248c1
oseyphine <josephine.legleut@gmail.com>
2017-12-05 15:17:55 +0400

    base admin ejs

--- config/routes/route.js
+++ config/routes/route.js
@@ -12,4 +12,25 @@ route.get('/register', (req, res) => {
   res.render('../pages/register.ejs')
 })
 
+// ADMIN
+route.get('/dashboard', (req, res) => {
+  res.render('../pages/admin/dashboard.ejs', { layout: '../layouts/admin' });
+})
+
+route.get('/newarticle', (req, res) => {
+  res.render('../pages/admin/newarticle.ejs', { layout: '../layouts/admin' })
+})
+
+route.get('/published', (req, res) => {
+  res.render('../pages/admin/published.ejs', { layout: '../layouts/admin' })
+})
+
+route.get('/draft', (req, res) => {
+  res.render('../pages/admin/draft.ejs', { layout: '../layouts/admin' })
+})
+
+route.get('/edithprofil', (req, res) => {
+  res.render('../pages/admin/edithprofil.ejs', { layout: '../layouts/admin' })
+})
+
 module.exports = route;
