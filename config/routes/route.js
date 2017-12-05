dddfb991a3e9ea6255ec0da4dc203be2c2db3fea
RemyMozelle <timoz974@hotmail.fr>
2017-12-05 15:16:24 +0400

    ajout des controlleurs users articles, model articles

--- config/routes/route.js
+++ config/routes/route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const route = express.Router();
-const users = require('../../app/models/users');
 
-console.log(users.getOne())
+const usersController = require('../../app/controllers/usersController');
+
+console.log(usersController.getOneUser())
 
 route.get('/', (req, res) => {
   res.render('../pages/home.ejs')
